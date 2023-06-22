8a66014e68dddbc20fb3bfbfc89edfc99017665f
Alpha Hinex <AlphaHinex@gmail.com>
2023-06-22 16:37:35 +0800

    Homework

--- 25/src/25/test01.py
+++ 25/src/25/test01.py
@@ -17,17 +17,21 @@ def save():
     money = int(request.form.get('money'))
 
     # 在下方写你的代码：设置session，保存用户存款金额
-    
-    return '成功存储了%s元' % money
+    if 'money'not in session:
+        session['money'] = [money]
+    else:
+        h = session['money']
+        h.append(money)
+        session['money'] = h
 
+    return '成功存储了%s元' % money
 
 @app.route('/query', methods=['GET'])
 def query():
     # 在下方写你的代码：获取session,计算总金额
-
-    return '余额:'
-
-
+    h = session.get('money')
+    count = sum(h)
+    return '余额:%s元' % count
 if __name__ == "__main__":
     app.run(
         host='127.0.0.1',
